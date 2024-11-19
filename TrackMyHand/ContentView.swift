651f4b9006b15c2feebd1ba6afe3f04f242dcdfb
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2024-11-19 02:18:42 -0500

    Initial Commit

---
+++ TrackMyHand/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackMyHand
+//
+//  Created by Laksh Bharani on 19/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackMyHand/TrackMyHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMyHandApp.swift
+//  TrackMyHand
+//
+//  Created by Laksh Bharani on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackMyHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
